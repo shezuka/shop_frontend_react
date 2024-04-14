740e7c45790925723139d6e801525d5d6555b83b
Dima <dima.horbal.dev@gmail.com>
2024-04-14 16:27:09 -0600

    fix login page

--- app/admin/(auth)/layout.tsx
+++ app/admin/(auth)/layout.tsx
@@ -4,8 +4,10 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="m-auto sm:max-w-full md:max-w-md lg:max-w-md flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      {children}
+    <div className="fixed top-0 bottom-0 left-0 right-0 bg-primary-900">
+      <div className="m-auto sm:max-w-full md:max-w-md lg:max-w-md flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        {children}
+      </div>
     </div>
   );
 }
