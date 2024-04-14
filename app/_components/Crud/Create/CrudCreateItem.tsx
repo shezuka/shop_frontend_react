938fa165a439ccea6bd47d23e0ee638c310181e0
Dima <dima.horbal.dev@gmail.com>
2024-04-14 16:44:25 -0600

    add error messages on item creation

--- app/_components/Crud/Create/CrudCreateItem.tsx
+++ app/_components/Crud/Create/CrudCreateItem.tsx
@@ -8,11 +8,13 @@ import CrudManageItem from "@/app/_components/Crud/Item/CrudManageItem";
 import LoaderSpinner from "@/app/_components/Loader/LoaderSpinner";
 import axios from "@/app/_utils/axios";
 import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
 
 function CrudCreateItem(props: CrudCreateItemPropsType) {
   const router = useRouter();
   const [loading, setLoading] = React.useState(false);
   const [itemData, setItemData] = React.useState({});
+  const [errors, setErrors] = React.useState<any[]>([]);
 
   const onSubmit = React.useCallback(
     (e: FormEvent) => {
@@ -22,18 +24,48 @@ function CrudCreateItem(props: CrudCreateItemPropsType) {
         .post(props.createItemApiUrl, itemData)
         .then(() => {
           router.push(props.onSuccessRedirectUri);
+          setErrors([]);
         })
-        .catch((e) => {
+        .catch((e: AxiosError) => {
           console.error(e);
           setLoading(false);
+          if (e.response) {
+            if (e.response.status === 400) {
+              const data = e.response.data as any;
+              setErrors([data.message]);
+            } else if (e.response.status === 422) {
+              let data = e.response.data as any;
+              data = data.detail.map((error: any) => {
+                const fieldName = error.loc[error.loc.length - 1];
+                const fieldData = props.fields.find(
+                  (it) => it.name === fieldName,
+                );
+                return `${fieldData?.label}: ${error.msg}`;
+              });
+              setErrors(data);
+            }
+          }
         });
     },
-    [props.createItemApiUrl, props.onSuccessRedirectUri, itemData, router],
+    [
+      props.fields,
+      props.createItemApiUrl,
+      props.onSuccessRedirectUri,
+      itemData,
+      router,
+    ],
   );
 
   return (
     <>
       <PageTitle>{props.pageTitle}</PageTitle>
+      {errors.length
+        ? errors.map((it) => (
+            <div key={it} className="text-red-500">
+              {it}
+            </div>
+          ))
+        : null}
       {loading ? (
         <div className="h-full flex justify-center items-center">
           <LoaderSpinner />
